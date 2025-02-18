b505ef00e93357dd7f43697cacf41cd647c8a586
LOUIS\LouisThomps <thompsonl0109@gmail.com>
2025-02-18 19:05:01 +0900

    blog posts succesfully pulls from mongo db

--- Personal-Portfolio-BlogServer/server/routes/main.js
+++ Personal-Portfolio-BlogServer/server/routes/main.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
-//const Post = require('../models/Post');
+const Post = require('../models/post');
 
-router.get('', (req,res) => {
+router.get('', async(req,res) => {
     const locals = {
      title: "Louis's NodeJS Blog",
      description: "Simple Blog created with NodesJs, Express & MonoDb."
+    }
+    //Get's blogs from db.
+    try{
+        const data = await Post.find();
+        res.render('index', {locals, data});
+
+    } catch (error) {
+        console.log(error);
     }
     
     res.render('index', { locals });
@@ -16,25 +24,38 @@ router.get('', (req,res) => {
 // Home
 //  */
 
-function insertPostData() {
-        Post.insertMany([
-        {
-            title: "The Golden Age Of Hip Hop",
-            body: "This is the body text"
-        },
-        {
-            title: "The Dark Age's Of Hip Hop",
-            body: "This is the body text"
-        },
-        {
-            title: "The Re-surgence Of Hip Hop",
-            body: "This is the body text"
-        },
-    ])
-}
-insertPostData();
 
 router.get('/about', (req,res) => {
     res.render('about');
 });
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+//////// Manual Insert 
+
+// function insertPostData() {
+//     Post.insertMany([
+//     {
+//         title: "The Golden Age Of Hip Hop",
+//         body: "This is the body text"
+//     },
+//     {
+//         title: "The Dark Age's Of Hip Hop",
+//         body: "This is the body text"
+//     },
+//     {
+//         title: "The Re-surgence Of Hip Hop",
+//         body: "This is the body text"
+//     },
+// ])
+// }
+//insertPostData();
